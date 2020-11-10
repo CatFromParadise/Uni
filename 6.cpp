49c0ead0130974f48481f5fbd9f0b512697bcbc7
CatFromParadise <Xidius1102@gmail.com>
2020-11-10 14:52:41 +0300

    Inital commit

---
+++ 6.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream> 
+#include <iomanip> 
+#include <cmath>
+
+using namespace std;
+
+void main() 
+{
+	double x, y;
+	cout << "Enter x" << endl;
+	cin >> x;
+	do {
+		y = pow(x, 5) + pow(2 * x, 2) - 3;
+		x=x+0.2;
+		cout <<left<<"y="<< y<<endl;
+	}
+	while (x >= 2 && x <= 4);
+	//s
+}
